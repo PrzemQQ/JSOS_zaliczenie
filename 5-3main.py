8c81a90873b82f1dbbf8081e98fba3affff55530
Przemek <przemeksipa14@gmail.com>
2021-11-16 10:40:50 +0100

    added comments

--- 5-3main.py
+++ 5-3main.py
@@ -1,4 +1,6 @@
 class Student:
+    """[klasa zawiera informacje o studencie]
+    """
     przedmiot1 = "analiza"
     przedmiot2 = "algebra"
     przedmiot3 = "fizyka"
@@ -12,18 +14,37 @@ class Student:
         self.indeks = indeks
         
     def dodaj_ocene(self,przedmiot,ocena):
+        """[Dodaje oceny do indeksu]
+
+        Args:
+            przedmiot ([type]): [string]
+            ocena ([type]): [int]
+
+        Returns:
+            [type]: [zwraca informacje o dodaniu oceny i dodaje ocene do listy]
+        """
+        self.ocena = ocena
+        self.przedmiot = przedmiot
         match przedmiot.lower():
             case "analiza":
-                self.oceny_p1.append(self.ocena)
+                self.oceny_p1.append(ocena)
             case "algebra":
-                self.oceny_p2.append(self.ocena)
+                self.oceny_p2.append(ocena)
             case "fizyka":
-                self.oceny_p3.append(self.ocena)
+                self.oceny_p3.append(ocena)
             case default:
                 pass
-        return f"Dodano ocenę {self.ocena} do przedmiotu {self.przedmiot}" 
+        return f"Dodano ocenę {ocena} do przedmiotu {przedmiot}" 
     
     def wyswietl_oceny(self,przedmiot):
+        """[wyswietla ocene]
+
+        Args:
+            przedmiot ([type]): [int]
+
+        Returns:
+            [list]: [wyswietla ocene z danego przedmiotu dla danego studenta]
+        """
         match przedmiot.lower():
             case "analiza":
                 return f"Oceny z {przedmiot.lower()}  dla ucznia {self.imie, self.nazwisko} =  {self.oceny_p1} "
@@ -42,4 +63,7 @@ anna_nowak = Student("Anna","Nowak",288564)
 joanna_mucha = Student("Joanna","Mucha",234086)
 
 #test metod dla JK
-print(jan_kowalski)
+if __name__ == '__main__':
+    print(jan_kowalski.dodaj_ocene("analiza",5))
+    print(jan_kowalski)
+    
